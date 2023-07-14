3fe07836c3d758f5f9ead141da29a732b9472b0a
Leonardo Santiago <leonardo201220@gmail.com>
2023-07-14 08:38:04 -0300

    Criação da base de dados games e model Game

---
+++ games/Game.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+const connection = require('../database/database');
+
+const Game = connection.define('games', {
+    title:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }, 
+    year: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }, 
+    price: {
+        type: Sequelize.DECIMAL(2,2),
+        allowNull: false
+    }
+});
+
+Game.sync({force: true});  
+
+
+module.exports = Game;
