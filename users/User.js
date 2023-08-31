00b9457e99b14b6a09a48db8c37f5920e1980c80
Leonardo Santiago <leonardo201220@gmail.com>
2023-08-31 15:08:24 -0300

    Criação do model User

---
+++ users/User.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const connection = require('../database/database');
+
+const User = connection.define('users', {
+    name:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }, 
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }, 
+    passwd: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+
+
+module.exports = Game;
